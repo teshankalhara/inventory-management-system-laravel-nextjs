8720a3d5c9468104d1e2df115362126abaff3dec
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2026-03-10 20:49:10 +0530

    Refactor UserController to use UserRequest for validation and improve response types

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Services\UserService;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
+    public function __construct(private readonly UserService $service)
+    {
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json($this->service->paginate());
     }
 
     /**
@@ -26,9 +31,10 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserRequest $request): JsonResponse
     {
-        //
+        $user = $this->service->create($request->validated());
+        return response()->json($user, 201);
     }
 
     /**
@@ -50,16 +56,18 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(UserRequest $request, int $id): JsonResponse
     {
-        //
+        $user = $this->service->update($id, $request->validated());
+        return response()->json($user);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroy(int $id): JsonResponse
     {
-        //
+        $this->service->delete($id);
+        return response()->json(['message' => 'User deleted.']);
     }
 }
