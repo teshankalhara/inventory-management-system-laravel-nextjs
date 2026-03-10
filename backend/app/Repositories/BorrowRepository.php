69e2e918a31611253d198d6b2924607447c690b9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2026-03-10 22:44:11 +0530

    Fix indentation in paginate method for improved readability

--- backend/app/Repositories/BorrowRepository.php
+++ backend/app/Repositories/BorrowRepository.php
@@ -15,7 +15,7 @@ class BorrowRepository extends UtilRepository
     public function paginate(int $perPage = 15, array $with = []): LengthAwarePaginator
     {
         return $this->model->with(['item', 'creator'])
-        ->orderByDesc('created_at')->paginate($perPage);
+            ->orderByDesc('created_at')->paginate($perPage);
     }
 
 
