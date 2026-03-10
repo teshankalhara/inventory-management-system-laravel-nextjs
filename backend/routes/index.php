2f96b4fd43e75df5bab54ee890126cb1396f4882
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2026-03-11 01:25:51 +0530

    Refactor routes in index.php to improve organization and add ActivityLogsController for activity log retrieval

--- backend/routes/index.php
+++ backend/routes/index.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ActivityLogsController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CupboardsController;
+use App\Http\Controllers\PlacesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,7 +14,12 @@ Route::post('/login', [AuthController::class, 'login']);
 //protctd routr
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me',      [AuthController::class, 'me']);
+    Route::get('/me', [AuthController::class, 'me']);
 
-
+    Route::middleware('role:admin')->group(function () {
+        Route::apiResource('users', UserController::class)->except(['show']);
+        Route::apiResource('cupboards', CupboardsController::class);
+        Route::apiResource('places', PlacesController::class);
+        Route::get('/activity-logs', [ActivityLogsController::class, 'index']);
+    });
 });
