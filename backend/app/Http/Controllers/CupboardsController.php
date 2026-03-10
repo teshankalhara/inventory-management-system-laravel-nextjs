1ad8061b4032e2fa5da299a0953d478c9b4a546b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2026-03-10 18:18:34 +0530

    Create CupboardsController with resource methods

---
+++ backend/app/Http/Controllers/CupboardsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cupboards;
+use Illuminate\Http\Request;
+
+class CupboardsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cupboards $cupboards)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cupboards $cupboards)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cupboards $cupboards)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cupboards $cupboards)
+    {
+        //
+    }
+}
