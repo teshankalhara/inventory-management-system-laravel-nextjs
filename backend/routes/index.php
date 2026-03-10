b44ab6dc451999affac3a23747e9ec1d29579883
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2026-03-10 22:31:46 +0530

    Add protected routes for logout and user information retrieval

--- backend/routes/index.php
+++ backend/routes/index.php
@@ -8,4 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 //protctd routr
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me',      [AuthController::class, 'me']);
 
+
+});
